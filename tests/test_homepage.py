a141ff46c64cb0642b59e85670a2623295466fbb
Savely <tosinbabasin@gmail.com>
2022-10-11 23:02:02 +0400

    Add selenium listener, update conftest.py, add delete cookie in seleniumbase.py, update test_homepage.py

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -7,15 +7,10 @@ from pom.homepage_nav import HomepageNav
 class TestHomepage:
     def test_nav_links(self):
         homepage_nav = HomepageNav(self.driver)
-        expected_links_text = homepage_nav.NAV_LINK_TEXT
 
-        actual_links_text  = homepage_nav.get_nav_links_text()
-
-        assert actual_links_text == expected_links_text, 'Validating Nav Links text'
-        
         homepage_nav.get_tiny_box_close_button().click()
         
         for indx in range(8):
             homepage_nav.get_nav_links()[indx].click()
-            homepage_nav.driver.delete_all_cookies()
-            time.sleep(3)
+            time.sleep(1.5)
+
